d40d11439f2084aa63c80118ba086c4c08fc2cfb
Iree Hale <LightWatch21@protonmail.com>
2021-02-10 18:09:04 -0700

    adding javascript portion of the project

--- scroll.js
+++ scroll.js
@@ -1,1 +1,57 @@
-console.log("hello world");
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+/*unsplash api*/
+const count = 10;
+const apiKey ='7RqnykV6HgIQyVDMbE9I8OBsbgWAnb2D80-1N2wzFp8';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+// create Elements for links and photos, add to DOM
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+        //item.setAttribute('href', photo.links.html);
+        //item.setAttribute('target', '_blank');
+        // create <img> for photo
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        //img.setAttribute('src', photo.urls.regular);
+        //img.setAttribute('alt', photo.alt_description);
+        //img.setAttribute('title', photo.alt_description);
+        // Put <img> inside <a>n then put both inside imageContainer Element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+// Get photos from unsplash API
+async function getPhotos() {
+    try{
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {
+        //catch error here
+    }
+}
+
+
+// On Load
+getPhotos();
